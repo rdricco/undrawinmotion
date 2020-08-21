ade3914efe20ddb1aad8d37465ef4f8b60401cd0
Renato Ricco <renato@ricco.dev.br>
2020-08-20 21:58:56 -0300

    teste

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,25 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Header from "../components/Header/index.svelte";
+  import Footer from "../components/Footer/index.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment}/>
+<Header {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
+
+<Footer />
