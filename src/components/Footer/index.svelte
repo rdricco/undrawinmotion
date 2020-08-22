f2f557ad7f83df33f63350b8ed54c14d5a449939
Renato Ricco <renato@ricco.dev.br>
2020-08-22 02:53:22 -0300

    initial footer styles

--- src/components/Footer/index.svelte
+++ src/components/Footer/index.svelte
@@ -1,8 +1,26 @@
-<footer>
-  <nav class="container">
-    <ul>
+<style>
+  #footer {
+    display: flex;
+    justify-content: center;
+  }
+  nav {
+    /* display: flex; */
+    justify-content: space-between;
+  }
+</style>
+
+<footer id="footer" class="flex bg__secondary-color">
+  <nav class="container flex">
+    <span>LOGO + © 2020 inMotion Rocks</span>
+    <ul class="flex">
       <li>
-        <a href="/">LOGO</a>
+        <a href="/">Github</a>
+      </li>
+      <li>
+        <a href="/">Twitter</a>
+      </li>
+      <li>
+        <a href="/">Instagram</a>
       </li>
     </ul>
   </nav>
