e0f97d06a37a4e322958e49743237bff37bb67a5
Renato Ricco <renato@ricco.dev.br>
2020-08-22 02:30:48 -0300

    Initial Hero

---
+++ src/components/Hero/index.svelte
@@ -0,0 +1,20 @@
+<style>
+  #hero {
+  }
+</style>
+
+<section id="hero">
+  <div class="container">
+    <p>
+      A community driven and
+      <br />
+      <strong>Open Source</strong>
+      collection of
+      <br />
+      animated
+      <strong>unDraw</strong>
+      illustrations!
+    </p>
+
+  </div>
+</section>
