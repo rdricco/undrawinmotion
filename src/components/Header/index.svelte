b9a47b35311247e5c5431c8340b8465af2add27b
Renato Ricco <renato@ricco.dev.br>
2020-08-22 02:31:42 -0300

    color + align

--- src/components/Header/index.svelte
+++ src/components/Header/index.svelte
@@ -6,7 +6,8 @@
   nav {
     border-bottom: 1px solid rgba(255, 62, 0, 0.1);
     font-weight: 300;
-    padding: 0 1em;
+    display: flex;
+    justify-content: space-between;
   }
 
   ul {
@@ -48,14 +49,12 @@
   }
 </style>
 
-<header id="header">
+<header id="header" class="bg__secondary-color">
   <nav class="container x-auto">
+    <a aria-current={segment === undefined ? 'page' : undefined} href=".">
+      inMotion Rocks
+    </a>
     <ul>
-      <li>
-        <a aria-current={segment === undefined ? 'page' : undefined} href=".">
-          LOGO
-        </a>
-      </li>
       <li>
         <a
           rel="prefetch"
